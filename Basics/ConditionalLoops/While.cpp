44be8e3bbec3ab4ba0ee12a5eb77c85c4a2bffde
HEETMEHTA18 <heetmehta18125@gmail.com>
2025-01-05 17:09:24 +0530

    Initial Commit

---
+++ Basics/ConditionalLoops/While.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int count=0;
+    while (count < 10)
+    {
+       
+        cout << "Current Count:"<<count << endl;
+    
+         count++;
+    }
+    return 0;
+}

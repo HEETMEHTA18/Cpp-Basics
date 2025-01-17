951bd137caf8817250ce2e53889a52c784ce88ab
Heet Mehta <heetmehta18125@gmail.com>
2025-01-17 21:13:57 +0530

    Create strlength.cpp

---
+++ Basics/String/strlength.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "Hello, World!";
+    cout << "Length of string: " << str.length() << endl;
+    cout << "Size of string: " << str.size() << endl;
+    return 0;
+}

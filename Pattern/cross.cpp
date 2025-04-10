3be96ce65e4e33352227f6e7743fded6049ec5f1
Heet Mehta <heetmehta18125@gmail.com>
2025-04-10 18:17:02 +0530

    Final Commit

--- Pattern/cross.cpp
+++ Pattern/cross.cpp
@@ -11,7 +11,7 @@ int main()
     {
         for(int j=1;j<=n;j++)
         {
-            if(i==mid || j==mid)
+            if(i==mid || j==mid+2)
             {
             cout<<"* ";
             }
